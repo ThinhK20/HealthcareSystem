bb14c5fbccb8f33a60eb65cdee7a5591cb0c40f5
ThinhK20 <74862129+ThinhK20@users.noreply.github.com>
2023-11-16 00:54:12 +0700

    Implement client base

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+
+function App() {
+   return (
+      <div className="text-blue-600 font-bold">
+         Healthcare System
+         <Button size="small">Learn More</Button>
+      </div>
+   );
+}
+
+export default App;
