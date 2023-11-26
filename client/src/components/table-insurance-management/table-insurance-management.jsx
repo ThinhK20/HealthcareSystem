4d25c6600d734000ca77c58bac50463eb83ef3d5
ttminh-blue <minhnhox792@gmail.com>
2023-11-26 18:01:02 +0700

    Get and delete insuarance policíe

--- client/src/components/table-insurance-management/table-insurance-management.jsx
+++ client/src/components/table-insurance-management/table-insurance-management.jsx
@@ -1,42 +1,88 @@
-import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faEdit} from '@fortawesome/free-solid-svg-icons'
 import {faTrash} from '@fortawesome/free-solid-svg-icons'
 import {faPlus} from '@fortawesome/free-solid-svg-icons'
+import axios from "axios";
+import { useState, useEffect } from "react";
 
+ 
 function TableInsuranceManagement() {
-return (
-<div className="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10">
-<button onClick={event => { popuphandler(true); }} className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 mt-4 sm:mt-0 inline-flex items-start justify-start px-6 py-3 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded">
-                        <p className="text-sm font-medium leading-none text-white"><FontAwesomeIcon icon={faPlus} /> New</p>
-                    </button>
+    const [data, setData] = useState([]);
 
-<table className="w-full table-fixed">
-    <thead>
-        <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">InsureID</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description</th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
-        </tr>
-    </thead>
-    <tbody className="bg-white divide-y divide-gray-200">
-        <tr>
-            <td className="px-6 py-4 whitespace-nowrap">001</td>
-            <td className="px-6 py-4 whitespace-nowrap">Letan</td>
-            <td className="px-6 py-4 whitespace-nowrap">P001</td>
-            <td className="px-6 py-4 whitespace-nowrap">
-            </td>
-            <td className="px-0 py-4 bg-gray-50 text-center sm:px-6">
-                <button className="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out"><FontAwesomeIcon icon= {faEdit} /></button>
-                <button className="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out"><FontAwesomeIcon icon= {faTrash} /></button>
-            </td>
-        </tr>
-    </tbody>
-</table>
-    
-</div>
-);
+    const authFetch = axios.create({
+        baseURL: 'https://localhost:44384/api',
+     });
+    const getData =  async () => {
+        const data = await authFetch.get("/InsuarancePolicy");
+        console.log(11111111, data.data);
+        setData(data.data);
+    }
+    const handleDelete = async (index) => {
+        console.log(index)
+        const api = await authFetch.delete(`/InsuarancePolicy/${index}`);
+        console.log(11111111, api)
+        const updatedData = data.filter(item => item.policyID !== index);
+        setData(updatedData);
+      };
+    useEffect(() => {
+        getData();
+     }, []);
+    return (
+        <>
+
+        <div className="container my-12 py-12 mx-auto px-4 md:px-6 lg:px-12">
+        <button type="button" className=" text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+        <FontAwesomeIcon icon={faPlus}/> New
+        </button>
+
+        <section className="mb-20 text-gray-800">
+            <div className="block rounded-lg shadow-lg bg-white">
+            <div className="flex flex-col">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="overflow-hidden">
+                    <table className="min-w-full mb-0">
+                        <thead className="border-b rounded-t-lg text-left">
+                        <tr>
+                            <th scope="col" className="rounded-tl-lg text-medium text-blue-600 font-medium px-6 py-4">Insuarance Policy ID</th>
+                            <th scope="col" className="text-medium text-blue-600 font-medium px-6 py-4">Name</th>
+                            <th scope="col" className="text-medium text-blue-600 font-medium px-6 py-4">Description</th>
+                            <th scope="col" className="rounded-tr-lg text-medium text-blue-600 font-medium px-6 py-4"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {data.map((item, index) => (
+                        <tr key={index} className={index % 2 === 0 ? 'border-b bg-gray-100' : 'border-b'}>
+                            <th className="text-sm font-medium px-6 py-4 whitespace-nowrap text-left" scope="row">
+                             {index + 1 }
+                            </th>
+                            <td className="text-sm font-normal px-6 py-4 whitespace-nowrap text-left text-gray-500">{item.name}</td>
+                            <td className="text-sm font-normal px-6 py-4 whitespace-nowrap text-left text-gray-500">{item.description}</td>
+                            <td className="text-large font-normal px-6 py-4 whitespace-nowrap text-right">
+                            <a href="#!" className="font-large text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 transition duration-300 ease-in-out">
+                                <FontAwesomeIcon icon={faEdit} />
+                            </a>
+                            <a onClick={() => handleDelete(item.policyID)} href="#!" className="font-large ml-4 text-red-600 hover:text-red-700 focus:text-red-700 active:text-red-800 transition duration-300 ease-in-out">
+                             <FontAwesomeIcon icon={faTrash} />
+                            </a>
+                            </td>
+                        </tr>
+                        ))}
+                        
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        
+        </section>
+        
+        </div>
+        </>
+        
+    );
 }
  
 export default TableInsuranceManagement;
